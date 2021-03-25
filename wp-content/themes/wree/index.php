3f68de044e939cca4dc15a3ba2d4d0d96f80ac62
Tristan Eason <tristaneason@protonmail.com>
2021-03-25 11:13:29 -0400

    Updates the theme index.php

---
+++ wp-content/themes/wree/index.php
@@ -0,0 +1,29 @@
+<?php
+if (is_front_page()) {
+    // do a thing
+}
+
+if (if_home()) {
+    // do a thing
+}
+
+if (is_single()) {
+    // do a thing
+}
+
+if (is_page()) {
+    // do a thing
+}
+
+if (is_author()) {
+    // do a thing
+}
+
+if (is_category()) {
+    // do a thing
+}
+
+if (is_tag()) {
+    // do a thing
+}
+?>
