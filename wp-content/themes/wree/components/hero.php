2a3f86ea5477cbadf9a0d3023d8325d594de0164
Tristan Eason <tristaneason@protonmail.com>
2021-05-19 11:49:51 -0400

    Updates as of May 17

--- wp-content/themes/wree/components/hero.php
+++ wp-content/themes/wree/components/hero.php
@@ -1,14 +1,25 @@
 <?php
 // Component: Hero
+
+$featured_post = get_field('featured_post_main');
+$featured_id = $featured_post->ID;
+$featured_date = date('F j, Y', strtotime($featured_post->post_date));
 ?>
 
-<section id="componentHero" class="flex align-center" style="background-image: url('<?php  ?>')">
+<style>
+    #componentHero {
+        background-image: url('<?= get_the_post_thumbnail_url($featured_id); ?>');
+        background-position: center;
+    }
+</style>
+
+<section id="componentHero" class="flex align-center">
     <div class="container flex column justify-center">
-        <h1><?php the_field('heading'); ?></h1>
-        <h2><?php the_field('subheading'); ?></h2>
-        <div class="cta flex">
-            <a href="<?php the_field('cta_url'); ?>" class="button large">
-                <?php the_field('cta_text'); ?>
+        <h1><?= get_the_title($featured_id); ?></h1>
+        <h2><?= get_the_excerpt($featured_id); ?></h2>
+        <div class="cta">
+            <a href="<?= get_permalink($featured_id); ?>" class="button large">
+                Read More
             </a>
         </div>
     </div>
