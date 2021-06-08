dd381994bc9d2b57124198e2bd4f82fffad25af5
Tristan Eason <tristaneason@protonmail.com>
2021-06-08 18:55:14 -0400

    Nav edits and article page completion

--- wp-content/themes/wree/components/sidebar.php
+++ wp-content/themes/wree/components/sidebar.php
@@ -2,6 +2,26 @@
 // Component: Sidebar
 ?>
 
-<aside id="sidebar">
-
+<aside class="sidebar">
+    <section class="meeting-section">
+        <h3>Monthly Meetings</h3>
+        <p>Join us! Monthly Meetings via ZOOM<br>@ 8:30 PM EST</p>
+        <p>Our next meeting is June 9, 2021</p>
+        <a href="/contact-wree/" class="button small">Contact us to join</a>
+    </section>
+    <section class="about-section">
+        <h3>What We Do</h3>
+        <p>Still committed to Peace as our first priority, WREE focuses on the many social issues that prevent Peace and Prosperity for all.There are many issues that need as much support and activism from our members and friends that they can provide. Community Volunteerism is powerful.</p>
+    </section>
+    <section class="contact-section">
+        <h3>Contact WREE</h3>
+        <span class="font-bold">Address</span>
+        <address>
+            1808 Hylan Blvd. Suite 1009<br>Staten Island, New York 10305
+        </address>
+        <span class="font-bold d-block">Email</span>
+        <a href="mailto:wree-info@usvanguard.net">wree-info@usvanguard.net</a>
+        <span class="font-bold d-block">Phone</span>
+        <a href="tel:501-492-6720">+1 (502) 492-6720</a>
+    </section>
 </aside>
--- wp-content/themes/wree/single.php
+++ wp-content/themes/wree/single.php
@@ -6,15 +6,24 @@
 
 <main id="article">
     <section class="container grid two-thirds">
-        <article class="">
-            <header>
+        <article class="post-container">
+            <header class="post-header">
                 <h1><?php the_title(); ?></h1>
+                <span>
+                    <?php the_date(); ?> by <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a>
+                </span>
+                <img src="<?php the_post_thumbnail_url() ?>" class="img-responsive" alt="">
             </header>
-            <footer></footer>
+            <div class="post-body">
+                <?php the_content(); ?>
+            </div>
+            <footer class="post-footer">
+                <span>More articles by <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></span>
+            </footer>
         </article>
-        <aside class="">
-            <h2>Sidebar</h2>
-        </aside>
+        <div class="">
+            <?php include theme_root('/components/sidebar.php'); ?>
+        </div>
     </section>
 </main>
 
